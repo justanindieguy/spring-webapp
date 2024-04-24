42cda635aaa80e1c9780b8882ae0d167b15dd621
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-04-24 13:26:37 -0600

    learn how to tell spring boot to do not create beans / auto-configure specific classes by using parameter exclude in @SpringBootApplication annotation

--- src/main/java/com/justanindieguy/webapp/WebappApplication.java
+++ src/main/java/com/justanindieguy/webapp/WebappApplication.java
@@ -3,6 +3,11 @@ package com.justanindieguy.webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+ * @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class }) <- Don't auto-configure this classes
+ * it is developer responsibility to create the Bean for this class/classes
+ */
+
 @SpringBootApplication
 public class WebappApplication {
 
