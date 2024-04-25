f4892681ddab08a8b951615527f22342047473d6
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-04-25 12:17:47 -0600

    create WebConfig file to handle redirection for static pages, add thymeleaf href tag to redirect to courses page

---
+++ src/main/java/com/justanindieguy/webapp/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.justanindieguy.webapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(@NonNull ViewControllerRegistry registry) {
+        registry.addViewController("/courses").setViewName("courses");
+    }
+
+}
