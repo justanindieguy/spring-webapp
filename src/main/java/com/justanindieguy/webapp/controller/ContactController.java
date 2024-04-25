3e96f9842230f47eab9295719c64c725aeb306a7
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-04-25 16:17:19 -0600

    refactor saveMessage PostMapping, establish dependency between ContactService and ContactController, use Spring's dependency injection

--- src/main/java/com/justanindieguy/webapp/controller/ContactController.java
+++ src/main/java/com/justanindieguy/webapp/controller/ContactController.java
@@ -1,17 +1,23 @@
 package com.justanindieguy.webapp.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.justanindieguy.webapp.models.Contact;
+import com.justanindieguy.webapp.services.ContactService;
 
 @Controller
 public class ContactController {
 
-    private static final Logger log = LoggerFactory.getLogger(ContactController.class);
+    private final ContactService contactService;
+
+    @Autowired
+    public ContactController(ContactService contactService) {
+        this.contactService = contactService;
+    }
 
     @RequestMapping("/contact")
     public String displayContactPage() {
@@ -19,15 +25,8 @@ public class ContactController {
     }
 
     @PostMapping("/saveMsg")
-    public ModelAndView saveMsg(@RequestParam String name, @RequestParam String mobileNum,
-            @RequestParam String email, @RequestParam String subject,
-            @RequestParam String message) {
-
-        log.info("Name: " + name);
-        log.info("Mobile number: " + mobileNum);
-        log.info("Email address: " + email);
-        log.info("Subject: " + subject);
-        log.info("Message: " + message);
+    public ModelAndView saveMessage(Contact contact) {
+        contactService.saveMessageDetails(contact);
 
         return new ModelAndView("redirect:/contact");
     }
