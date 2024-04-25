fc226b4fc0ec1b2e12d9753a939e0faf9082550f
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-04-25 14:43:46 -0600

    create method to handle POST contact form http request, by now it is just logging the provided values

--- src/main/java/com/justanindieguy/webapp/controller/ContactController.java
+++ src/main/java/com/justanindieguy/webapp/controller/ContactController.java
@@ -1,14 +1,35 @@
 package com.justanindieguy.webapp.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ContactController {
 
+    private static final Logger log = LoggerFactory.getLogger(ContactController.class);
+
     @RequestMapping("/contact")
     public String displayContactPage() {
         return "contact.html";
+    }
+
+    @PostMapping("/saveMsg")
+    public ModelAndView saveMsg(@RequestParam String name, @RequestParam String mobileNum,
+            @RequestParam String email, @RequestParam String subject,
+            @RequestParam String message) {
+
+        log.info("Name: " + name);
+        log.info("Mobile number: " + mobileNum);
+        log.info("Email address: " + email);
+        log.info("Subject: " + subject);
+        log.info("Message: " + message);
+
+        return new ModelAndView("redirect:/contact");
     }
 
 }
