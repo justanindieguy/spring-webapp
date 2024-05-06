342326cba1b8cfce474c9be0067d1fe002d4784f
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-05-06 13:42:42 -0600

    configure spring security to allow free access to specified routes

--- src/main/java/com/justanindieguy/webapp/config/ProjectSecurityConfig.java
+++ src/main/java/com/justanindieguy/webapp/config/ProjectSecurityConfig.java
@@ -11,7 +11,14 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll())
+        http.authorizeHttpRequests(requests -> requests
+                .requestMatchers("/", "/home").permitAll()
+                .requestMatchers("/holidays").permitAll()
+                .requestMatchers("/contact").permitAll()
+                .requestMatchers("/saveMsg").permitAll()
+                .requestMatchers("/courses").permitAll()
+                .requestMatchers("/about").permitAll()
+                .requestMatchers("/assets/**").permitAll())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
 
