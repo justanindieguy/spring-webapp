4d7d1bc2e2b92c49303f46247aeb7ff6a1989eb0
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-05-09 11:01:18 -0600

    update spring security configuration to work together with H2 Database: allow access to h2-console, allow frames to run h2-console

--- src/main/java/com/justanindieguy/eazyschool/config/ProjectSecurityConfig.java
+++ src/main/java/com/justanindieguy/eazyschool/config/ProjectSecurityConfig.java
@@ -1,31 +1,41 @@
 package com.justanindieguy.eazyschool.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 public class ProjectSecurityConfig {
 
     @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.ignoringRequestMatchers("saveMsg"))
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http,
+            HandlerMappingIntrospector introspector) throws Exception {
+
+        MvcRequestMatcher.Builder mvcMatcherBuilder = new MvcRequestMatcher.Builder(introspector);
+
+        http.csrf(csrf -> csrf.ignoringRequestMatchers(mvcMatcherBuilder.pattern("saveMsg"))
+                .ignoringRequestMatchers(PathRequest.toH2Console()))
                 .authorizeHttpRequests(requests -> requests
-                        .requestMatchers("/dashboard").authenticated()
-                        .requestMatchers("/", "/home").permitAll()
-                        .requestMatchers("/holidays").permitAll()
-                        .requestMatchers("/contact").permitAll()
-                        .requestMatchers("/saveMsg").permitAll()
-                        .requestMatchers("/courses").permitAll()
-                        .requestMatchers("/about").permitAll()
-                        .requestMatchers("/login").permitAll()
-                        .requestMatchers("/logout").permitAll()
-                        .requestMatchers("/assets/**").permitAll())
+                        .requestMatchers(mvcMatcherBuilder.pattern("/dashboard")).authenticated()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/home")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/holidays")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/contact")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/saveMsg")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/courses")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/about")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/login")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/logout")).permitAll()
+                        .requestMatchers(mvcMatcherBuilder.pattern("/assets/**")).permitAll()
+                        .requestMatchers(PathRequest.toH2Console()).permitAll())
                 .formLogin(loginConfigurer -> loginConfigurer
                         .loginPage("/login")
                         .defaultSuccessUrl("/dashboard")
@@ -34,6 +44,8 @@ public class ProjectSecurityConfig {
                         .logoutSuccessUrl("/login?logout=true")
                         .invalidateHttpSession(true).permitAll())
                 .httpBasic(Customizer.withDefaults());
+
+        http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()));
 
         // ! Deny all requests inside the web application.
         // http.authorizeHttpRequests(requests -> requests.anyRequest().denyAll())
